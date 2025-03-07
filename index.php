421e720ab10eb3584fa603114c6e9f51ff295942
thiran-naleendra <60607966+thiran-naleendra@users.noreply.github.com>
2025-03-07 10:06:03 +0530

    update nav

--- index.php
+++ index.php
@@ -55,10 +55,15 @@
             <div class="site-header header-style-1">
                 <nav class="navigation navbar navbar-default">
                     <div class="container-fluid">
-                        <div class="navbar-header">
-
-                            <a class="navbar-brand" href="index.php"><img src="assets/images/logo.png" alt=""></a>
-                        </div>
+                    <div class="navbar-header">
+                        <button type="button" class="open-btn">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.php"><img src="assets/images/logo.png" alt=""></a>
+                    </div>
                         <div id="navbar" class="navbar-collapse collapse navigation-holder">
                             <button class="close-navbar"><i class="ti-close"></i></button>
                             <?php
--- nav.php
+++ nav.php
@@ -1,16 +1,22 @@
 <ul class="nav navbar-nav">
-    <li class="menu-item-has-children">
+    <li >
         <a href="index.php">Home</a>
     </li>
 
-    <li><a href="destination.php">Destination</a></li>
+    <li>
+        <a href="destination.php">Destination</a>
+    </li>
 
-    <li class="menu-item-has-children">
+    <li >
         <a href="service.php">Service</a>
     </li>
-    <li class="menu-item-has-children">
+
+    <li >
         <a href="blog-fullwidth.html">Blog </a>
 
     </li>
-    <li><a href="contact.html">Contact</a></li>
+
+    <li>
+        <a href="contact.html">Contact</a>
+    </li>
 </ul>
